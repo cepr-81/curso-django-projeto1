b7868a7cc64ebacf6d845f53d5745de08187e9f1
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2022-06-23 21:33:46 -0300

    Criação do header

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
+    return render(request, 'recipes/pages/home.html')
