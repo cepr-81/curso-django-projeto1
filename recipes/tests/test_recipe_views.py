afe25c7b0aebe4efdbf10302aa72731fc9a30eeb
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2022-08-08 00:10:17 -0300

    tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,6 +4,9 @@ from recipes import views
 
 
 class RecipeViewTest(TestCase):
+
+    # Views tests
+
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -15,3 +18,29 @@ class RecipeViewTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    # Responses tests
+
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes yet! :(',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_category_view_returns_status_code_200_ok(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, 404)
