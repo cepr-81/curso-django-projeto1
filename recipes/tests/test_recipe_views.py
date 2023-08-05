110bcfede36224ff6b9ac36faccdfbd06ca272b8
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2023-08-04 23:54:53 -0300

    TDD Updates

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -122,3 +122,13 @@ class RecipeViewTest(RecipeTestBase):
         url = reverse('recipes:search')
         view = resolve(url)
         self.assertIs(view.func, views.search)
+
+    def test_recipe_search_loads_correct_template(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
