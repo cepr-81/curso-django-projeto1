a3722be79a263a15761354e99f201e639cd78a80
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2023-08-08 19:55:10 -0300

    Pagination Updates

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,8 @@ from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
+from utils.pagination import make_pagination
+
 from .models import Recipe
 
 
@@ -10,8 +12,11 @@ def home(request):
         is_published=True,
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9, 4)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
     })
 
 
@@ -23,8 +28,11 @@ def category(request, category_id):
             is_published=True,
         ).order_by('-id'))
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9, 4)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -52,9 +60,13 @@ def search(request):
         is_published=True,
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, 9, 4)
+
     contexto = {
         "page_title": f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     }
     return render(request, 'recipes/pages/search.html', contexto)
