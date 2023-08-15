da4282d86ea50977f4bd47462fcd156094270c69
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2023-08-15 08:26:01 -0300

    Form updates

--- authors/forms.py
+++ authors/forms.py
@@ -11,3 +11,31 @@ class RegisterForm(forms.ModelForm):
                   'email',
                   'password',
                   ]
+        # exclude = ['first_name']
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'Type a valid e-mail.'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field can not be empty.',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password'
+            })
+        }
