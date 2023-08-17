6538ada5a1f8f87ffe37357aaeb3c6ab6159f342
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2023-08-17 10:10:40 -0300

    Form updates

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,48 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password can not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The minimal length '
+            'should be at least 8 characters.'
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+        label=('Confirm Password')
+    )
+
     class Meta:
         model = User
         fields = ['first_name',
@@ -39,3 +79,25 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password'
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não utilize a palavra "atenção" como senha',
+                code='invalid',
+                params={'value': 'atenção'}
+            )
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Passwords must be equal.',
+                'password2': 'Passwords must be equal.',
+            })
