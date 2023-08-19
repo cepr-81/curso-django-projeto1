6246445c5a8c28450e19cdc1f72eeef4a9abb97a
Carlos Eduardo Ramos <eduardo@atlantisdobrasil.com>
2023-08-19 09:23:47 -0300

    Form updates

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -12,6 +14,18 @@ def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError(('Password must have at least one uppercase '
+                              'letter, one lowercase letter and one number. '
+                               'The minimal length should be at least '
+                               '8 characters.'),
+                              code='Invalid'
+                              )
+
+
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,6 +33,8 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_attr(self.fields['first_name'], 'required', True)
+        add_attr(self.fields['last_name'], 'required', True)
 
     password = forms.CharField(
         required=True,
@@ -32,7 +48,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The minimal length '
             'should be at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
 
     password2 = forms.CharField(
--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -21,6 +22,10 @@ def register_create(request):
 
     POST = request.POST
     request.session['register_form_data'] = POST
-    # form = RegisterForm(POST)
+    form = RegisterForm(POST)
 
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user was created, please log in.')
+        del (request.session['register_form_data'])
     return redirect('authors:register')
